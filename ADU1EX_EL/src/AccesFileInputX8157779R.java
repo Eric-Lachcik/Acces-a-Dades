a316d2aaaf1624614e126a23fdb961cc7af5f7ec
Eric-Lachcik <Lachcikeric@gmail.com>
2024-09-30 14:23:08 +0200

    Some Changes

--- ADU1EX_EL/src/AccesFileInputX8157779R.java
+++ ADU1EX_EL/src/AccesFileInputX8157779R.java
@@ -1,25 +1,24 @@
-import com.sun.security.jgss.GSSUtil;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-import java.io.*;
 public class AccesFileInputX8157779R {
+    private String filePath;
 
-    public int ByteReader(String variable){
-        InputStream input = new FileInputStream(variable);
-
-        int data = 0;
-        try {
-            data = input.read();
-            while(data != -1) {
-
-                data = input.read();
-            }
-            input.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-            System.out.println("Ha Fallado el FileInputStream");
-            System.out.print(e.getMessage());
-        }
-        return data;
+    public AccesFileInputX8157779R(String filePath) {
+        this.filePath = filePath;
     }
 
+    public void readFile() throws IOException {
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            int data;
+            System.out.println("Reading file byte by byte:");
+            while ((data = fis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            System.out.println(); // Newline after reading
+        } catch (IOException e) {
+            throw new IOException("File cannot be read (byte by byte).");
+        }
+    }
 }
+

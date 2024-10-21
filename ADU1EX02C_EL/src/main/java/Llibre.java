db0cff6d3113a6cb848b5ae64a31365bfcb43b42
Eric-Lachcik <lachcikeric@gmail.com>
2024-10-21 18:46:42 +0200

    EX3

---
+++ ADU1EX02C_EL/src/main/java/Llibre.java
@@ -0,0 +1,13 @@
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+public class Llibre {
+    private String autor;
+    private String titol;
+    private Integer any;
+    private String resum;
+
+    public Llibre() {}
+}

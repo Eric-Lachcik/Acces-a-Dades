0016bb2058520a073639761878d83b6057007ef7
Eric-Lachcik <lachcikeric@gmail.com>
2025-03-13 17:45:30 +0100

    Project Created ADU7

---
+++ ADU7EX01_EL-Franky/src/main/java/com/example/ADU7EX01_EL_Franky/Adu7Ex01ElFrankyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ADU7EX01_EL_Franky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adu7Ex01ElFrankyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adu7Ex01ElFrankyApplication.class, args);
+	}
+
+}
---
+++ ADU7EX01_EL-Franky/src/test/java/com/example/ADU7EX01_EL_Franky/Adu7Ex01ElFrankyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ADU7EX01_EL_Franky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adu7Ex01ElFrankyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

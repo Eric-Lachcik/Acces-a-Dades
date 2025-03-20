4115250892e9dc3165ee859edc5503d761f273fe
Eric-Lachcik <lachcikeric@gmail.com>
2025-03-20 17:40:20 +0100

    First Mods for franky

--- ADU7EX01_EL-Franky/src/main/java/com/example/ADU7EX01_EL_Franky/Adu7Ex01ElFrankyApplication.java
+++ ADU7EX01_EL-Franky/src/main/java/com/example/ADU7EX01_EL_Franky/Adu7Ex01ElFrankyApplication.java
@@ -2,11 +2,14 @@ package com.example.ADU7EX01_EL_Franky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class Adu7Ex01ElFrankyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Adu7Ex01ElFrankyApplication.class, args);
 	}
 
